076d8bb0d898b807bac1b1663365f7936f3217d3
Declan W <148910875+DeclanW1@users.noreply.github.com>
2025-09-30 21:03:43 +0100

    Changed the hight of the tiles to be longer

--- src/app/main-page/components/TileGrid.tsx
+++ src/app/main-page/components/TileGrid.tsx
@@ -3,14 +3,11 @@ import TileBase from "./TileBase";
 export default function TileGrid() {
   return (
     <div
-      className="grid gap-4
-                 grid-cols-1 md:grid-cols-3
-                 auto-rows-[12rem]">
+      className="grid gap-4 grid-cols-1 md:grid-cols-3auto-rows-[14rem]">
 
       <div className="md:col-span-2 md:row-span-2 [&>*]:h-full">
         <TileBase title="CV" href="/cv" image="/icons/cv.png" />
       </div>
-
 
       <div className="[&>*]:h-full [&>*]:aspect-auto">
         <TileBase title="About" href="/about" image="/icons/aboutme.png" />
@@ -18,7 +15,6 @@ export default function TileGrid() {
       <div className="[&>*]:h-full [&>*]:aspect-auto">
         <TileBase title="Contact" href="/contact" image="/icons/contact.png" />
       </div>
-
 
       <div className="[&>*]:h-full [&>*]:aspect-auto">
         <TileBase title="Skills" href="/skills" image="/icons/skills.png" />
